b1bcb7bc6bc013998eaee460942c55b4121b5e87
adibrastegarnia <adib.rastegarnia@intel.com>
2025-04-01 12:52:20 -0700

    Set project ID in deployment service controller

--- app-interconnect/internal/controller/network/deploymentservice_controller.go
+++ app-interconnect/internal/controller/network/deploymentservice_controller.go
@@ -183,8 +183,7 @@ func (c *DeploymentServiceController) bindServices(ctx context.Context, deployme
 		return nil
 	}
 
-	// TODO: Get the project ID from resource labels.
-	var projectID clusterclient.ProjectID
+	projectID := clusterclient.ProjectID(deploymentCluster.Labels[utils.AppOrchProjectIDLabel])
 	clusterID := clusterclient.ClusterID(deploymentCluster.Spec.ClusterID)
 
 	clusterConfig, err := c.Clusters.GetClusterConfig(ctx, clusterID, projectID)
