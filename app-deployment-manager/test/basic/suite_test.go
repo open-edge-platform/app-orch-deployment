54ac85081d86daa08c5b7fe43c65a77c1335bc08
Maldonado Vega, Ruben <ruben.maldonado.vega@intel.com>
2025-04-03 14:06:44 -0700

    add adm list deployment test. enable arm

---
+++ app-deployment-manager/test/basic/suite_test.go
@@ -0,0 +1,79 @@
+// SPDX-FileCopyrightText: 2025-present Intel Corporation
+//
+// SPDX-License-Identifier: Apache-2.0
+
+// Package basic is a suite of basic functionality tests for the ADM service
+package basic
+
+import (
+	"context"
+	"fmt"
+	"os/exec"
+
+	"github.com/open-edge-platform/app-orch-deployment/app-deployment-manager/test/auth"
+	"github.com/stretchr/testify/suite"
+	"testing"
+	"time"
+)
+
+const (
+	RestAddress            = "app-deployment-api-rest-proxy:8081/"
+	RestAddressPortForward = "127.0.0.1"
+	KeycloakServer         = "keycloak.kind.internal"
+
+	PortForwardServiceNamespace = "orch-app"
+	PortForwardService          = "svc/app-deployment-api-rest-proxy"
+	PortForwardLocalPort        = "8081"
+	PortForwardAddress          = "0.0.0.0"
+	PortForwardRemotePort       = "8081"
+)
+
+// TestSuite is the basic test suite
+type TestSuite struct {
+	suite.Suite
+	KeycloakServer          string
+	DeploymentRESTServerUrl string
+	token                   string
+	projectID               string
+}
+
+// SetupSuite sets-up the integration tests for the ADM basic test suite
+func (s *TestSuite) SetupSuite() {
+	s.KeycloakServer = KeycloakServer
+	s.DeploymentRESTServerUrl = RestAddress
+}
+
+// SetupTest sets up for each integration test
+func (s *TestSuite) SetupTest() {
+	var err error
+	s.token = auth.SetUpAccessToken(s.T(), s.KeycloakServer)
+	s.DeploymentRESTServerUrl = fmt.Sprintf("http://%s:%s", RestAddressPortForward, PortForwardRemotePort)
+	s.projectID, err = auth.GetProjectId(context.TODO())
+	s.NoError(err)
+}
+
+func killportForwardToADM(cmd *exec.Cmd) error {
+	fmt.Println("kill process that port-forwards network to app-deployment-manager")
+	if cmd != nil && cmd.Process != nil {
+		return cmd.Process.Kill()
+	}
+	return nil
+}
+
+func portForwardToADM() (*exec.Cmd, error) {
+	fmt.Println("port-forward to app-deployment-manager")
+
+	cmd := exec.Command("kubectl", "port-forward", "-n", PortForwardServiceNamespace, PortForwardService, fmt.Sprintf("%s:%s", PortForwardLocalPort, PortForwardRemotePort), "--address", PortForwardAddress)
+	err := cmd.Start()
+	time.Sleep(5 * time.Second) // Give some time for port-forwarding to establish
+
+	return cmd, err
+}
+
+func TestTestSuite(t *testing.T) {
+	suite.Run(t, new(TestSuite))
+}
+
+// TearDownTest tears down remnants of each integration test
+func (s *TestSuite) TearDownTest(ctx context.Context) {
+}
