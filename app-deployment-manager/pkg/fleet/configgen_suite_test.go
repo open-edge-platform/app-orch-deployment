c45f19c3c423fbd157a5eafaf66c296b99197d2c
Maldonado Vega, Ruben <ruben.maldonado.vega@intel.com>
2025-04-17 13:08:54 -0700

    fix conflicts from main

--- app-deployment-manager/pkg/fleet/configgen_suite_test.go
+++ app-deployment-manager/pkg/fleet/configgen_suite_test.go
@@ -39,7 +39,7 @@ var _ = BeforeSuite(func() {
 	testEnv = &envtest.Environment{
 		CRDDirectoryPaths: []string{
 			filepath.Join("../..", "config", "crd", "bases"),
-			filepath.Join("test", "crd"),
+			filepath.Join("../..", "pkg", "fleet", "test", "crd"),
 		},
 		ErrorIfCRDPathMissing: true,
 	}
