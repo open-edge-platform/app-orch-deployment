059c3a424a1fdbfcdc2bfaf620b99319563a70b2
Maldonado Vega, Ruben <ruben.maldonado.vega@intel.com>
2025-04-17 13:05:13 -0700

    fix conflicts from main

---
+++ app-deployment-manager/controllers/apiextension/suite_test.go
@@ -0,0 +1,118 @@
+// SPDX-FileCopyrightText: (C) 2023 Intel Corporation
+//
+// SPDX-License-Identifier: Apache-2.0
+
+package apiextension
+
+import (
+	"context"
+	"path/filepath"
+	"testing"
+
+	ctrl "sigs.k8s.io/controller-runtime"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+
+	"github.com/open-edge-platform/app-orch-deployment/app-deployment-manager/api/v1beta1"
+	fleetv1alpha1 "github.com/rancher/fleet/pkg/apis/fleet.cattle.io/v1alpha1"
+	"k8s.io/client-go/kubernetes/scheme"
+	"k8s.io/client-go/rest"
+	"k8s.io/client-go/tools/record"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/envtest"
+	logf "sigs.k8s.io/controller-runtime/pkg/log"
+	"sigs.k8s.io/controller-runtime/pkg/log/zap"
+	metricsserver "sigs.k8s.io/controller-runtime/pkg/metrics/server"
+
+	"github.com/open-edge-platform/app-orch-deployment/app-deployment-manager/internal/config"
+	//+kubebuilder:scaffold:imports
+)
+
+// These tests use Ginkgo (BDD-style Go testing framework). Refer to
+// http://onsi.github.io/ginkgo/ to learn more about Ginkgo.
+
+var (
+	cfg       *rest.Config
+	k8sClient client.Client
+	testEnv   *envtest.Environment
+	ctx       context.Context
+	cancel    context.CancelFunc
+)
+
+func TestAPIs(t *testing.T) {
+	RegisterFailHandler(Fail)
+
+	RunSpecs(t, "Controller Suite")
+}
+
+var _ = BeforeSuite(func() {
+	logf.SetLogger(zap.New(zap.WriteTo(GinkgoWriter), zap.UseDevMode(true)))
+
+	ctx, cancel = context.WithCancel(context.TODO())
+
+	By("bootstrapping test environment")
+	testEnv = &envtest.Environment{
+		CRDDirectoryPaths: []string{
+			filepath.Join("../..", "config", "crd", "bases"),
+			filepath.Join("../..", "pkg", "fleet", "test", "crd"),
+		},
+		ErrorIfCRDPathMissing: true,
+	}
+
+	var err error
+	// cfg is defined in this file globally.
+	cfg, err = testEnv.Start()
+	Expect(err).NotTo(HaveOccurred())
+	Expect(cfg).NotTo(BeNil())
+
+	err = v1beta1.AddToScheme(scheme.Scheme)
+	Expect(err).NotTo(HaveOccurred())
+	err = fleetv1alpha1.AddToScheme(scheme.Scheme)
+	Expect(err).NotTo(HaveOccurred())
+
+	//+kubebuilder:scaffold:scheme
+
+	k8sClient, err = client.New(cfg, client.Options{Scheme: scheme.Scheme})
+	Expect(err).NotTo(HaveOccurred())
+	Expect(k8sClient).NotTo(BeNil())
+
+	k8sManager, err := ctrl.NewManager(cfg, ctrl.Options{
+		Scheme:  scheme.Scheme,
+		Metrics: metricsserver.Options{BindAddress: "0"},
+	})
+	Expect(err).ToNot(HaveOccurred())
+
+	// Set config before the controllers start to prevent error
+	err = config.SetAPIExtensionConfig(&config.APIExtensionConfig{
+		IngressKind:          "nginx",
+		APIProxyService:      "api-proxy",
+		APIProxyPort:         int32(8123),
+		APIGroupDomain:       "orchestrator-extension.apis",
+		APIAgentChartRepo:    "https://test-repoo",
+		APIAgentChart:        "api-agent",
+		APIAgentChartVersion: "0.1.0",
+		APIAgentNamespace:    "orch-app",
+	})
+	Expect(err).ShouldNot(HaveOccurred())
+
+	err = (&Reconciler{
+		Client:   k8sManager.GetClient(),
+		Scheme:   k8sClient.Scheme(),
+		recorder: record.NewFakeRecorder(10),
+	}).SetupWithManager(k8sManager)
+	Expect(err).ToNot(HaveOccurred())
+
+	go func() {
+		defer GinkgoRecover()
+		err = k8sManager.Start(ctx)
+		Expect(err).ToNot(HaveOccurred(), "failed to run manager")
+	}()
+})
+
+var _ = AfterSuite(func() {
+	cancel()
+	By("tearing down the test environment")
+	err := testEnv.Stop()
+	Expect(err).NotTo(HaveOccurred())
+})
