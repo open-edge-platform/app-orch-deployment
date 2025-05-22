cefde3b670e221903346db1a322a2f16838184f3
adibrastegarnia <adib.rastegarnia@intel.com>
2025-05-22 13:34:57 -0700

    trigger loop early

--- app-deployment-manager/controllers/deployment/deployment_controller.go
+++ app-deployment-manager/controllers/deployment/deployment_controller.go
@@ -988,12 +988,13 @@ func (r *Reconciler) updateDeploymentStatus(d *v1beta1.Deployment, grlist []flee
 		appName := getAppNameForGitRepo(&gitrepo, d.GetId())
 
 		if d.Status.DeployInProgress {
-			// Check if the GitRepo is in Stalled state
 			if gitrepo.Status.Summary.DesiredReady != 1 &&
 				(gitrepo.Status.GitJobStatus == "Current" || gitrepo.Status.GitJobStatus == "InProgress") {
-				stalledApps = true
-
+				r.requeueStatus = true
+				return
 			}
+			
+			// Check if the GitRepo is in Stalled state
 
 			if sc, ok := utils.GetGenericCondition(&gitrepo.Status.Conditions, "Stalled"); ok && sc.Status == corev1.ConditionTrue {
 				stalledApps = true
