318e7f3893e9d07f50fc77b5d16afde83f41aa97
adibrastegarnia <adib.rastegarnia@intel.com>
2025-05-27 10:50:11 -0700

    keep the current status

--- app-deployment-manager/controllers/deployment/deployment_controller.go
+++ app-deployment-manager/controllers/deployment/deployment_controller.go
@@ -1066,7 +1066,7 @@ func (r *Reconciler) updateDeploymentStatus(d *v1beta1.Deployment, grlist []flee
 		newState = v1beta1.Unknown
 	case clustercounts.Total == 0:
 		if gitReposInTransition {
-			newState = v1beta1.Error
+			newState = d.Status.State
 		} else {
 			// Wait specified interval after creation before showing NoTargetClusters,
 			// to give Fleet + ADM a chance to bootstrap the Deployment.
