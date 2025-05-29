87bf77838452d73d00896a108549fbc4f773763a
adibrastegarnia <adib.rastegarnia@intel.com>
2025-05-29 12:04:33 -0700

    fix

--- app-deployment-manager/test/deployment/create_deployment_test.go
+++ app-deployment-manager/test/deployment/create_deployment_test.go
@@ -25,7 +25,6 @@ const (
 )
 
 func (s *TestSuite) TestCreateTargetedDeployment() {
-	s.T().Skip()
 	for _, app := range []string{AppWordpress, AppNginx} {
 		_, code, err := utils.StartDeployment(s.AdmClient, app, DeploymentTypeTargeted, DeploymentTimeout)
 		s.Equal(http.StatusOK, code)
@@ -34,8 +33,6 @@ func (s *TestSuite) TestCreateTargetedDeployment() {
 }
 
 func (s *TestSuite) TestCreateAutoScaleDeployment() {
-	s.T().Skip()
-
 	for _, app := range []string{AppWordpress, AppNginx} {
 		_, code, err := utils.StartDeployment(s.AdmClient, app, DeploymentTypeAutoScaling, DeploymentTimeout)
 		s.Equal(http.StatusOK, code)
@@ -44,7 +41,6 @@ func (s *TestSuite) TestCreateAutoScaleDeployment() {
 }
 
 func (s *TestSuite) TestCreateDiffDataDeployment() {
-	s.T().Skip()
 	originalDpConfigs := CopyOriginalDpConfig(utils.DpConfigs)
 	defer func() { utils.DpConfigs = CopyOriginalDpConfig(originalDpConfigs) }()
 
@@ -62,7 +58,7 @@ func (s *TestSuite) TestCreateDiffDataDeployment() {
 	s.Equal(http.StatusOK, code)
 	s.NoError(err, "Failed to create '"+AppWordpress+"-"+DeploymentTypeTargeted+"' deployment")
 }
-func (s *TestSuite) TestRetrieveDeploymentStatus() {
+func (s *TestSuite) TestRetrieveDeploymentStatusWithNoLabels() {
 	status, code, err := utils.GetDeploymentsStatus(s.AdmClient, nil)
 	s.NoError(err)
 	s.Equal(http.StatusOK, code, "Failed to retrieve deployments status")
@@ -99,4 +95,8 @@ func (s *TestSuite) TestDeploymentStatusWithLabelsFilter() {
 	s.Equal(http.StatusOK, code, "Failed to retrieve deployments status with labels filter")
 	s.Equal(int32(2), *status.Running, "Running deployments count mismatch")
 	s.Equal(int32(2), *status.Total, "Total deployments count mismatch with labels filter")
+}
+
+func (s *TestSuite) TestDeploymentStateCountsVerification() {
+	s.T().Skip()
 }
