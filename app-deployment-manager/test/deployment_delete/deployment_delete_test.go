01c9ff5b2ed02bf929b3d76905fe6e7f9684dd9a
adibrastegarnia <adib.rastegarnia@intel.com>
2025-06-04 11:09:04 -0700

    Add Delete deployment test suite

---
+++ app-deployment-manager/test/deployment_delete/deployment_delete_test.go
@@ -0,0 +1,19 @@
+// SPDX-FileCopyrightText: 2025-present Intel Corporation
+//
+// SPDX-License-Identifier: Apache-2.0
+
+package deployment
+
+import (
+	deploymentutils "github.com/open-edge-platform/app-orch-deployment/test-common-utils/pkg/deployment"
+	"github.com/open-edge-platform/orch-library/go/pkg/errors"
+)
+
+func (s *TestSuite) TestDeleteNonExistentDeployment() {
+	s.T().Parallel()
+	// Attempt to delete a deployment that does not exist
+	deploymentID := "non-existent-deployment"
+	err := deploymentutils.DeleteDeployment(s.AdmClient, deploymentID)
+	s.Equal(true, errors.IsNotFound(err))
+	s.T().Logf("successfully handled deletion of non-existent deployment with ID: %s", deploymentID)
+}
