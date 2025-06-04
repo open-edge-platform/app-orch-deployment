681b26ad194a4eec49c8ea4c67cb7c6f0f054d7b
adibrastegarnia <adib.rastegarnia@intel.com>
2025-06-04 11:56:10 -0700

    fix

--- app-deployment-manager/test/deployment_delete/deployment_delete_test.go
+++ app-deployment-manager/test/deployment_delete/deployment_delete_test.go
@@ -14,6 +14,7 @@ func (s *TestSuite) TestDeleteNonExistentDeployment() {
 	// Attempt to delete a deployment that does not exist
 	deploymentID := "non-existent-deployment"
 	status, err := deploymentutils.DeleteDeployment(s.AdmClient, deploymentID)
+	s.T().Log(err)
 	s.NoError(err)
 	s.Equal(http.StatusNotFound, status, "Expected HTTP status 404 for non-existent deployment deletion")
 	s.T().Logf("successfully handled deletion of non-existent deployment with ID: %s", deploymentID)
