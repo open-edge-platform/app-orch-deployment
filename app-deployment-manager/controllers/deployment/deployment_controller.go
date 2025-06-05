7d2a5e3cbec763b5145676b67ff377b669bd5620
adibrastegarnia <adib.rastegarnia@intel.com>
2025-06-05 16:41:33 -0700

    fix

--- app-deployment-manager/controllers/deployment/deployment_controller.go
+++ app-deployment-manager/controllers/deployment/deployment_controller.go
@@ -1013,6 +1013,7 @@ func (r *Reconciler) updateDeploymentStatus(ctx context.Context, d *v1beta1.Depl
 				fmt.Println("Test Jobs", job.Name)
 				// If the job is not completed, we are still waiting for it to finish
 				if job.Status.Succeeded == 0 && gitrepo.Status.GitJobStatus != "Failed" {
+					fmt.Println("Test git repo status message:", gitrepo.Status.Display.Message)
 					gitRepoInTransitionStatus = true
 				}
 
