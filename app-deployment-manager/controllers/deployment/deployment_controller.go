388112bb982282d62cf3eef46d1da535d1edad1c
adibrastegarnia <adib.rastegarnia@intel.com>
2025-06-06 09:49:49 -0700

    fix

--- app-deployment-manager/controllers/deployment/deployment_controller.go
+++ app-deployment-manager/controllers/deployment/deployment_controller.go
@@ -1143,13 +1143,14 @@ func (r *Reconciler) updateDeploymentStatus(ctx context.Context, d *v1beta1.Depl
 	}
 	fmt.Println("Test before final message:", d.Status.Message)
 	if gitRepoInTransitionStatus {
-		fmt.Println("Test git repo in transition status", d.Status.Message)
-		if d.Status.Message != "" {
+		fmt.Println("Test git repo in transition status, returning", d.Status.Message)
+		return
+		/*if d.Status.Message != "" {
 			fmt.Println("Test git repo in transition status", d.Status.Message)
 			d.Status.Display = fmt.Sprintf("Clusters: %v/%v/%v/%v, Apps: %v", clustercounts.Total, clustercounts.Running, clustercounts.Down, clustercounts.Unknown, apps)
 			d.Status.Summary = clustercounts
 			d.Status.State = newState
-		}
+		}*/
 	} else {
 		fmt.Println("Test git repo not in transition status", d.Status.Message)
 		d.Status.Display = fmt.Sprintf("Clusters: %v/%v/%v/%v, Apps: %v", clustercounts.Total, clustercounts.Running,
