d9e1a4e4d59e7802d8c2ac5587c7873e902451ab
pudelkoM <pudelkoM@users.noreply.github.com>
2025-06-11 14:35:16 +0200

    fix list test

--- app-deployment-manager/test/deployment_list/deployment_list_test.go
+++ app-deployment-manager/test/deployment_list/deployment_list_test.go
@@ -40,7 +40,39 @@ func (s *TestSuite) TestListDeploymentsWithPagination() {
 	s.NoError(err, "Failed to list deployments with pagination")
 	s.Equal(http.StatusOK, code, "Expected HTTP status 200 for listing deployments with pagination")
 	s.NotEmpty(deps, "Expected non-empty deployments list")
-	s.Len(deps, 1, "Expected exactly one deployment in the list")
+	s.Len(*deps, 1, "Expected exactly one deployment in the list")
+
+	for _, app := range []string{deploymentutils.AppWordpress, deploymentutils.AppNginx} {
+		displayName := deploymentutils.FormDisplayName(app, testName)
+		err := deploymentutils.DeleteAndRetryUntilDeleted(s.AdmClient, displayName, deploymentutils.RetryCount, deploymentutils.DeleteTimeout)
+		s.NoError(err)
+	}
+}
+
+func (s *TestSuite) TestListDeploymentsWithFilter() {
+	s.T().Parallel()
+	testName := "ListDeploymentsWithFilter"
+	for _, app := range []string{deploymentutils.AppWordpress, deploymentutils.AppNginx} {
+		deploymentReq := deploymentutils.StartDeploymentRequest{
+			AdmClient:         s.AdmClient,
+			DpPackageName:     app,
+			DeploymentType:    deploymentutils.DeploymentTypeTargeted,
+			DeploymentTimeout: deploymentutils.DeploymentTimeout,
+			DeleteTimeout:     deploymentutils.DeleteTimeout,
+			TestName:          testName,
+		}
+		_, code, err := deploymentutils.StartDeployment(deploymentReq)
+		s.Equal(http.StatusOK, code)
+		s.NoError(err, "Failed to create '"+app+"-"+deploymentutils.DeploymentTypeTargeted+"' deployment")
+	}
+
+	deps, code, err := deploymentutils.DeploymentsListWithParams(s.AdmClient, &restClient.DeploymentServiceListDeploymentsParams{
+		Filter: ptr("deployPackage = wordpress"),
+	})
+	s.NoError(err, "Failed to list deployments with filter")
+	s.Equal(http.StatusOK, code, "Expected HTTP status 200 for listing deployments with filter")
+	s.NotEmpty(deps, "Expected non-empty deployments list")
+	s.Len(*deps, 1, "Expected exactly one deployment in the list")
 
 	for _, app := range []string{deploymentutils.AppWordpress, deploymentutils.AppNginx} {
 		displayName := deploymentutils.FormDisplayName(app, testName)
