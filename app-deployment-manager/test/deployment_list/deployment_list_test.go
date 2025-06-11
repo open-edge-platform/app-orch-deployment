925ec2ffee4c807d39a50117e103b8f376c20efd
pudelkoM <pudelkoM@users.noreply.github.com>
2025-06-11 14:00:18 +0200

    wip2

---
+++ app-deployment-manager/test/deployment_list/deployment_list_test.go
@@ -0,0 +1,50 @@
+// SPDX-FileCopyrightText: (C) 2025-present Intel Corporation
+//
+// SPDX-License-Identifier: Apache-2.0
+
+package deployment_list
+
+import (
+	"net/http"
+
+	"github.com/open-edge-platform/app-orch-deployment/app-deployment-manager/api/nbi/v2/pkg/restClient"
+	deploymentutils "github.com/open-edge-platform/app-orch-deployment/test-common-utils/pkg/deployment"
+)
+
+func ptr[T any](v T) *T {
+	return &v
+}
+
+func (s *TestSuite) TestListDeploymentsWithPagination() {
+	s.T().Parallel()
+	testName := "ListDeploymentsWithPagination"
+	for _, app := range []string{deploymentutils.AppWordpress, deploymentutils.AppNginx} {
+		deploymentReq := deploymentutils.StartDeploymentRequest{
+			AdmClient:         s.AdmClient,
+			DpPackageName:     app,
+			DeploymentType:    deploymentutils.DeploymentTypeTargeted,
+			DeploymentTimeout: deploymentutils.DeploymentTimeout,
+			DeleteTimeout:     deploymentutils.DeleteTimeout,
+			TestName:          testName,
+		}
+		_, code, err := deploymentutils.StartDeployment(deploymentReq)
+		s.Equal(http.StatusOK, code)
+		s.NoError(err, "Failed to create '"+app+"-"+deploymentutils.DeploymentTypeTargeted+"' deployment")
+	}
+
+	deps, code, err := deploymentutils.DeploymentsListWithParams(s.AdmClient, &restClient.DeploymentServiceListDeploymentsParams{
+		PageSize: ptr(int32(1)),
+		Offset:   ptr(int32(0)),
+		OrderBy:  ptr("deployPackage"),
+	})
+	s.NoError(err, "Failed to list deployments with pagination")
+	s.Equal(http.StatusOK, code, "Expected HTTP status 200 for listing deployments with pagination")
+	s.NotEmpty(deps, "Expected non-empty deployments list")
+	s.Len(deps, 1, "Expected exactly one deployment in the list")
+
+	for _, app := range []string{deploymentutils.AppWordpress, deploymentutils.AppNginx} {
+		displayName := deploymentutils.FormDisplayName(app, testName)
+		err := deploymentutils.DeleteAndRetryUntilDeleted(s.AdmClient, displayName, deploymentutils.RetryCount, deploymentutils.DeleteTimeout)
+		s.NoError(err)
+	}
+}
